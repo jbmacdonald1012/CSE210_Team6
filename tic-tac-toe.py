14778830807bb127c9a67f599bacf107d92050ed
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2022-04-28 00:03:40 -0600

    create function to show users the game board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,12 +6,30 @@ def main():
     welcome()
     game = buildGameboard()
 
+    showGameboard(game)
+
 
 def welcome():
     print('Let\'s play Tic Tac Toe!')
     print()
 
 def buildGameboard():
+    gameboard = []
+
+    for area in range(9):
+        gameboard.append(area + 1)
+    return gameboard
+
+def showGameboard(game):
+    print()
+    print()
+    print(f'{game[0]} | {game[1]} | {game[2]}')
+    print('---------')
+    print(f'{game[3]} | {game[4]} | {game[5]}')
+    print('---------')
+    print(f'{game[6]} | {game[7]} | {game[8]}')
+    print()
+    print()
 
 
 
