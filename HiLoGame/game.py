c4ba648bd433299ead8a74c32072742b58d575c9
JD4440 <104406337+JD4440@users.noreply.github.com>
2022-05-09 20:45:49 -0600

    Update game.py

--- HiLoGame/game.py
+++ HiLoGame/game.py
@@ -1,29 +1,28 @@
+import random
+
 class cards:
     """ A set of cards that be drawn to provide a random integer between 1 and 13
-    The resonsibilty of the cards is to keep track of the cards drawn and calculate 
-    the points for it
 
     Attributes:
         value (int): The number on the card that is drawn."""
 
     def __init__(self):
         """Constructs a new instance of cards.
-
         Args:
             self (cards): An instance of cards.
         """
         
         self.value = 0
         self.roundscore = 0
-        
-"""    
+      
+    """    
     Constructor:
-        - cardValue = ''
+        - cardvalue = ''
         - roundScore = '' 
         - totalScore = 300
 
     Methods:
-        - drawCard(cardValue)
+        - drawCard(cardvalue)
             -Random integer from 1 - 13
             -display card
         
@@ -38,7 +37,19 @@ class cards:
         -restartValues (roundScore, totalScore)
             -totalScore: 300
             -roundScore: 0                    
-"""
+     """
+    
+    def drawcard(self):
+        """Generates a new random value
+        Args:
+            self (card): An instance of card.
+        """
+        self.cardvalue = random.randint(1, 13)
+        print(f'The card is {self.cardvaule}')
+
+    def updatescore(self):
+        
+
 
 class player:
 
