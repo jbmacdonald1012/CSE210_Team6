abd0134c59f5aad1ea3a018581cf12201190742b
JD4440 <104406337+JD4440@users.noreply.github.com>
2022-05-23 09:12:13 -0600

    Update terminal_service.py

--- Jumpers/game/terminal_service.py
+++ Jumpers/game/terminal_service.py
@@ -17,17 +17,7 @@ class TerminalService:
         """
         return input(prompt)
 
-    def read_number(self, prompt):
-        """Gets numerical input from the terminal. Directs the user with the given prompt.
 
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            prompt (string): The prompt to display on the terminal.
-
-        Returns:
-            float: The user's input as a number.
-        """
-        return float(input(prompt))
         
     def write_text(self, text):
         """Displays the given text on the terminal. 
