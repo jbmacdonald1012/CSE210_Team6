4fb63cd240cb2cc7ff6f7974f9fe9af2c47876ce
albertoalmada <albertoalmada@yahoo.com>
2022-05-23 23:35:11 -0300

    words fixed to work with jumper.py

--- Jumpers/game/words.py
+++ Jumpers/game/words.py
@@ -1,10 +1,12 @@
 import random
 
 class Words:
-    _wordList = ['sense', 'cause', 'other', 'power', 'point', 'plain', 'union', 'third', 'every', 'local', 'which', 'judge', 'place', 'small', 'grant', 'state', 'among', 'right', 'under', 'great', 'grown']
     
-    def selectRandom(_wordList):
-        return random.choice(_wordList).upper()
+    def __init__ (self):
+        self._wordList = ['sense', 'cause', 'other', 'power', 'point', 'plain', 'union', 'third', 'every', 'local', 'which', 'judge', 'place', 'small', 'grant', 'state', 'among', 'right', 'under', 'great', 'grown']
+    
+    def selectRandom(self):
+        return random.choice(self._wordList).upper()
     
     #this print fucntion was create to check the random words selection
     #print("The word is: ", selectRandom(_wordList))
