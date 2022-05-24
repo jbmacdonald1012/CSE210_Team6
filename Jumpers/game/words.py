f48bae99629731eb5c1694a95e16056daea57324
albertoalmada <albertoalmada@yahoo.com>
2022-05-23 23:43:10 -0300

    words file revised

--- Jumpers/game/words.py
+++ Jumpers/game/words.py
@@ -2,11 +2,10 @@ import random
 
 class Words:
     
-    def __init__ (self):
+    def __init__(self):
         self._wordList = ['sense', 'cause', 'other', 'power', 'point', 'plain', 'union', 'third', 'every', 'local', 'which', 'judge', 'place', 'small', 'grant', 'state', 'among', 'right', 'under', 'great', 'grown']
     
     def selectRandom(self):
         return random.choice(self._wordList).upper()
     
-    #this print fucntion was create to check the random words selection
-    #print("The word is: ", selectRandom(_wordList))
+  
