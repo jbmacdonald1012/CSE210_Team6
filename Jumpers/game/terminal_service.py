e7bffd4e79b1cb335785e24dfe06b7a708190288
JD4440 <104406337+JD4440@users.noreply.github.com>
2022-05-24 17:23:21 -0600

    Update terminal_service.py

--- Jumpers/game/terminal_service.py
+++ Jumpers/game/terminal_service.py
@@ -1,41 +1,56 @@
 from jumper import jumper
 
-    
+word = ""
+hidden_word = ""
+hidden_word_string = ""
+hidden_word_string = hidden_word_string.join(hidden_word)
+wrong_guesses = ""
+
 class TerminalService:
     def __init__(self):
         self.word = ""
 
     jumper = jumper()
 
+    def display_hidden_word(self, word, hidden_word):
+        for i in range (len(word)):
+            hidden_word.append("_ ")
+        return(hidden_word) 
+    
     def letter_guess(self, prompt):
-        """Gets text input from the terminal. Directs the user with the given prompt.
-
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            prompt (string): The prompt to display on the terminal.
-
-        Returns:
-            string: The user's input as text.
-        """
         
         letter_input = input("\nGuess a letter [a-z]: ")
+            
+        for i in range (len(word)):
+            if letter_input.lower() == word[i]:
+                hidden_word.pop(i)
+                hidden_word.insert(i, letter_input.upper() + " ")
+        
+        if letter_input not in word:            
+            if letter_input.lower() not in wrong_guesses:
+                print(f"\nNope, {letter_input.upper()} is not there!")
+                wrong_guesses.append(letter_input.lower())
+            else:
+                print(f"You have already guessed that {letter_input.lower()}!")
+        
+        if len(wrong_guesses) > 0:
+            print(f"Current wrong guesses: {wrong_guesses}")
+                  
+        print()
+        hidden_word_string = ""
+        hidden_word_string = hidden_word_string.join(hidden_word)
+        print(hidden_word_string)
 
-        while (len(letter_input.lower()) > 1) or not (letter_input.isalpha()):
-            if (len(letter_input.lower()) > 1):
-                letter_input = input("\nHow about just one letter?: ")
+        return wrong_guesses
 
-            if not letter_input.isalpha():
-                letter_input = input("\nDo you know what a letter is? Try again: ")
-
-
-
-    def write_text(self, text):
-        """Displays the given text on the terminal. 
-
-        Args: 
-            self (TerminalService): An instance of TerminalService.
-            text (string): The text to display.
-        """
-        print(text)
+    def game_over(self, is_playing, hidden_word):
+        _checker = "_ "
+        if _checker in hidden_word:
+            _is_playing = True
+        else:
+            _is_playing = False
+            print("\nCongratulations! You Win!")
+            exit()
+        return _is_playing
 
    
