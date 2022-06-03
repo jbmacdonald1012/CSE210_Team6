9029dc9db5a2618d2b7ad7549c4539d90937a681
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2022-06-03 00:03:02 -0600

    Add director class template to templates folder

---
+++ Templates/director-template.py
@@ -0,0 +1,16 @@
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            
+        """
+        
