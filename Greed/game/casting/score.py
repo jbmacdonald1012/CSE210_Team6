094a15c627336e4288043201c85fbacca8e3e4ab
JD4440 <104406337+JD4440@users.noreply.github.com>
2022-06-04 16:05:25 -0600

    Update score.py

--- Greed/game/casting/score.py
+++ Greed/game/casting/score.py
@@ -1,31 +1,31 @@
 from game.casting.actor import Actor
 
 
-class Artifact(Actor):
+class Score(Actor):
     """
     An item of cultural or historical interest. 
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The responsibility of an Artifact is to provide a score about itself.
 
     Attributes:
-        _message (string): A short description about the artifact.
+        _score (string): A short description about the artifact.
     """
     def __init__(self):
         super().__init__()
-        self._message = ""
+        self._score = ""
         
-    def get_message(self):
-        """Gets the artifact's message.
+    def get_score(self):
+        """Gets the artifact's score.
         
         Returns:
-            string: The message.
+            string: The score.
         """
-        return self._message
+        return self._score
     
-    def set_message(self, message):
-        """Updates the message to the given one.
+    def set_score(self, score):
+        """Updates the score to the given one.
         
         Args:
-            message (string): The given message.
+            score (string): The given score.
         """
-        self._message = message
+        self._score = score
