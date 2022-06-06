725ce855593524aedfe22b6ac94721ede2e909e8
albertoalmada <albertoalmada@yahoo.com>
2022-06-06 15:55:21 -0300

    change colors

--- Greed/game/shared/color.py
+++ Greed/game/shared/color.py
@@ -5,31 +5,31 @@ class Color:
     convenience methods for comparing them and converting to a tuple.
 
     Attributes:
-        _red (int): The red value.
+        _yellow (int): The yellow value.
         _green (int): The green value.
         _blue (int): The blue value.
         _alpha (int): The alpha or opacity.
     """
     
-    def __init__(self, red, green, blue, alpha = 255):
+    def __init__(self, yellow, green, blue, alpha = 255):
         """Constructs a new Color using the specified red, green, blue and alpha values. The alpha 
         value is the color's opacity.
         
         Args:
-            red (int): A red value.
+            yellow (int): A yellow value.
             green (int): A green value.
             blue (int): A blue value.
             alpha (int): An alpha or opacity.
         """
-        self._red = red
+        self._yellow = yellow
         self._green = green
         self._blue = blue 
         self._alpha = alpha
 
     def to_tuple(self):
-        """Gets the color as a tuple of four values (red, green, blue, alpha).
+        """Gets the color as a tuple of four values (yellow, green, blue, alpha).
 
         Returns:
             Tuple(int, int, int, int): The color as a tuple.
         """
-        return (self._red, self._green, self._blue, self._alpha)   
+        return (self._yellow, self._green, self._blue, self._alpha)   
