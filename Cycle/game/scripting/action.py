1c1a57e35269abd1e5f66e7fc521c007520d66ad
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2022-06-19 01:48:39 -0600

    Create action class

---
+++ Cycle/game/scripting/action.py
@@ -0,0 +1,14 @@
+class Action:
+    """ A thing that is done.
+
+    Completes something integral to the game. One method is used and should be overridden by derived classes.
+    """
+
+    def execute(self, cast, script):
+        """ Executes something integral to the game. 
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of actions in the game.
+        """
+        pass
