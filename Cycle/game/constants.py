cbc5ab69395a9d75ce29a4d82cc7218a642aa79b
albertoalmada <albertoalmada@yahoo.com>
2022-06-20 19:32:14 -0300

    colors point and constants file

--- Cycle/game/constants.py
+++ Cycle/game/constants.py
@@ -1,13 +1,13 @@
+from game.shared.color import Color
 
-
-COLUMNS = 40
-ROWS = 20
+COLUMNS = 50
+ROWS = 30
 CELL_SIZE = 15
 MAX_X = 900
 MAX_Y = 600
 FRAME_RATE = 15
 FONT_SIZE = 15
-CAPTION = "Snake"
+CAPTION = "Cycle"
 CYCLE_LENGTH = 8
 WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
