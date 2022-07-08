a3351444d588b41c05d0d8da9ba1afc875f996c6
JD4440 <104406337+JD4440@users.noreply.github.com>
2022-07-07 21:58:53 -0600

    Update move_paddle_action.py

--- AirHockey/game/scripting/move_paddle_action.py
+++ AirHockey/game/scripting/move_paddle_action.py
@@ -19,38 +19,18 @@ class MovePaddleAction(Action):
         position = position.add(velocity)
 
 
-        # if x < 0:
-        #     position = Point(0, position.get_y())
-        # elif x > (SCREEN_WIDTH - PADDLE_WIDTH):
-        #     position = Point(SCREEN_WIDTH - PADDLE_WIDTH, position.get_y())
-        # elif y < 0:
-        #     position = Point(position.get_x(), 0)
-        # elif y > (SCREEN_HEIGHT - PADDLE_HEIGHT):
-        #     position = Point(position.get_x(), SCREEN_HEIGHT - PADDLE_HEIGHT)
+        if x < 0:
+            position = Point(0, position.get_y())
+        elif x > (SCREEN_WIDTH - PADDLE_WIDTH):
+            position = Point(SCREEN_WIDTH - PADDLE_WIDTH, position.get_y())
+        elif y < 0:
+            position = Point(position.get_x(), 0)
+        elif y > (SCREEN_HEIGHT - PADDLE_HEIGHT):
+            position = Point(position.get_x(), SCREEN_HEIGHT - PADDLE_HEIGHT)
             
         body.set_position(position)
 
-    def execute(self, cast, script, callback):
-        paddle2 = cast.get_first_actor(PADDLE_TWO_GROUP)
-        body2 = paddle2.get_body()
-        velocity2 = body2.get_velocity()
-        position2 = body2.get_position()
-        x2 = position2.get_x()
-        y2 = position2.get_y()
-        
-        position2 = position2.add(velocity2)
-
-        if x2 < 0:
-            position2 = Point(0, position2.get_y())
-        elif x2 > (SCREEN_WIDTH - PADDLE_WIDTH):
-            position2 = Point(SCREEN_WIDTH - PADDLE_WIDTH, position2.get_y())
-        elif y2 < 0:
-            position2 = Point(position2.get_x(), 0)
-        elif y2 > (SCREEN_HEIGHT - PADDLE_HEIGHT):
-            position2 = Point(position2.get_x(), PADDLE_HEIGHT - PADDLE_HEIGHT)
-            
-        body2.set_position(position2)
-
+    
 
 
 
